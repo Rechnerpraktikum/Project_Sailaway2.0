73866e9d4406d0b3f5ca310a0d1763241c99789c
max <max@137.208.183.98>
2016-11-16 19:20:49 +0100

    funk folder added

---
+++ Funk Files/VoiceChat_Comm/src/ErrorMessage.java
@@ -0,0 +1,8 @@
+
+import javax.swing.JOptionPane;
+
+public class ErrorMessage {
+    public static void main(String args[]){
+        JOptionPane.showMessageDialog(new JOptionPane(),"This is a library, not a program.\nPlease run VoiceChat_Server or VoiceChat_Client");
+    }
+}
---
+++ Funk Files/VoiceChat_Server/src/Log.java
@@ -0,0 +1,5 @@
+public class Log {
+    private static String log="";
+    public static void add(String s){log+=s+"\n";}
+    public static String get(){return log;}
+}
